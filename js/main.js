6584ca307c65f5c031218df139a395ca486503a3
Stefan Koshy <skoshy@nextjump.com>
2017-01-04 16:30:39 -0500

    Added Chrome Sync support, plus support for setting a name

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
+// Get settings
+var name;
+
+chrome.storage.sync.get('name', function (result) {
+	if (typeof result.name == "undefined") {
+		changeName();
+	} else {
+		name = result.name;
+	}
+});
+
 $( document ).ready(function() {
 	var interval = setInterval(function() {
 		var momentNow = moment();
 		$('.time').html(momentNow.format('hh:mm'));
 	}, 100);
-	$('.time').fadeIn();
+	$('.name').html(name);
+	$('.name').click(function() {
+		changeName();
+	});
+	$('.centered_box').fadeIn();
 	/*
 		location stuff to use eventually eventually
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -11,3 +26,11 @@ $( document ).ready(function() {
 		});
 	*/
 });
+
+function changeName() {
+	name = window.prompt("What's your name?", "User");
+	if (name != null) {
+		chrome.storage.sync.set({'name': name}, function() {});
+		$('.name').html(name);
+	}
+}
