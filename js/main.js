983e0f5a2921d59f819ea6ad2b5996ee676c35cf
Stefan Koshy <skoshy@nextjump.com>
2017-01-04 16:19:39 -0500

    Bigger fonts, and working towards adding weather integration

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $( document ).ready(function() {
 		$('.time').html(momentNow.format('hh:mm'));
 	}, 100);
 	$('.time').fadeIn();
+	/*
+		location stuff to use eventually eventually
+		navigator.geolocation.getCurrentPosition(function(position) {
+			do_something(position.coords.latitude, position.coords.longitude);
+		});
+	*/
 });
