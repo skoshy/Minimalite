5666204d9e29024a85698d07bc0c65f1fca03044
Stefan Koshy <noreply@skoshy.com>
2017-01-05 02:08:40 -0500

    Allow storing notes. Still imperfect; breaks with HTML tags and new lines don't work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 // Get settings
-var name;
+var prefs = {};
 
 chrome.storage.sync.get('name', function (result) {
 	if (typeof result.name == "undefined") {
-		saveName("User");
+		saveSetting("User", "name");
 	} else {
-		name = result.name;
+		prefs.name = result.name;
+	}
+});
+
+chrome.storage.sync.get('notes', function (result) {
+	if (typeof result.notes == "undefined" || result.notes == "") {
+		saveSetting("Type notes here", "notes");
+	} else {
+		prefs.notes = result.notes;
 	}
 });
 
@@ -25,21 +33,38 @@ $( document ).ready(function() {
 		}
 		$('.welcome_text').html(newWelcomeText);
 	}, 100);
-	$('.name').html(name);
+	$('.name').html(prefs.name);
+	$('.notes').html(prefs.notes);
 	$('.centered_box, .wallpaper').fadeIn();
 
 	/* Set up saving your name by editing the name box */
-	$('.name').on('input', function(e) {
+	$('[contentEditable]').on('input', function(e) {
+		console.log(e);
+		el = $(e.target);
+		contentType = el.attr('data-content-type');
+
 		// only keep text, trim it, then shorten it to a max of 30 characters
-		newName = $.trim($(e.target).text());
-		if (newName == "") {
-			newName = "User";
+		newText = el.text().replace(/(?:\r\n|\r|\n)/g, '<br />');
+		if (contentType == "name") {
+			if (newText == "") {
+				newText = "User";
+			}
+			newText = newText.substring(0, 30);
+		} else if (contentType == "notes") {
+			if (newText == "") {
+				newText = "Type notes here";
+			}
 		}
-		$(e.target).html(newName.substring(0, 30));
+		el.html(newText);
 	});
-	$('.name').blur(function(e) {
-		newName = $.trim($(e.target).text());
-		saveName(newName);
+	$('[contentEditable]').blur(function(e) {
+		el = $(e.target);
+		contentType = el.attr('data-content-type');
+
+		newText = $.trim(el.text());
+		el.html(newText);
+
+		saveSetting(newText, contentType);
 	});
 
 	/*
@@ -50,8 +75,10 @@ $( document ).ready(function() {
 	*/
 });
 
-function saveName(newName) {
-	name = newName;
-	chrome.storage.sync.set({'name': name}, function() {});
-	return name;
+function saveSetting(newText, key) {
+	prefs[key] = newText;
+	newSetting = {};
+	newSetting[key] = newText;
+	chrome.storage.sync.set(newSetting, function() {});
+	return newText;
 }
