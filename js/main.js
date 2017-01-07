f973f09c4c539ae579a7aff4e6dd386e2a41523d
Stefan Koshy <noreply@skoshy.com>
2017-01-07 17:57:52 -0500

    Fixed weather issues. Fixed text input issues for all fields. Added wallpaper picker. Made preferences look nicer. Cleaned up code a bit. Ready for production!

--- js/main.js
+++ js/main.js
@@ -4,44 +4,72 @@ var settings = {
 	"contentTypes": {
 		"name": {
 			"allowNewlines": false,
-			"default": "User",
+			"default": "Human",
 			"maxCharacters": 30,
 			"class": "name"
 		},
 		"notes": {
 			"default": "Type notes here",
-			"class": "notes"
+			"class": "notes",
+			"allowNewlines": true
 		},
 		"weather_location": {
-			"default": "New York",
+			"default": "",
 			"allowNewlines": false,
 			"maxCharacters": 30
 		},
 		"custom_image": {
 			"default": "https://source.unsplash.com/category/nature/1920x1080/daily",
 			"class": "custom_image_field",
-			"allowNewLines": false
+			"allowNewlines": false
 		},
 		"blur": {
-			"default": "false",
+			"default": false,
 			"class": "blur",
 			"type": "checkbox"
 		}
 	}
 };
 
+var wallpapers = [
+	{
+		image: 'https://source.unsplash.com/1K5FKIyKVxQ'
+	},
+	{
+		image: 'https://source.unsplash.com/duiETcZN7O4'
+	},
+	{
+		image: 'https://source.unsplash.com/6xh7H5tWj9c'
+	},
+	{
+		image: 'https://source.unsplash.com/NDuPLKYRXQU'
+	},
+	{
+		image: 'https://source.unsplash.com/qLhCKmBjTec'
+	},
+	{
+		image: 'https://source.unsplash.com/7cdFZmLlWOM'
+	},
+	{
+		image: 'https://source.unsplash.com/0A_b9G-Rm6w'
+	},
+	{
+		image: 'https://source.unsplash.com/fWTVzP0os44'
+	}
+];
+
 var weather = {
 	timeUpdated: "",
-	location: "New York", // default
 
-	city: "",
+	weather_location: "New York",
+	weather_location_id: "",
 	currently: "",
-	image: "",
 	low: "",
 	high: "",
 	temp: "",
-	todayCode: "",
-	wind: {}
+	condition_code: "",
+	wind_speed: "",
+	wind_degrees: ""
 };
 
 // Load in all data into prefs object
@@ -74,7 +102,11 @@ chrome.storage.local.get(Object.keys(weather), function (data) {
 		!dataExists || // if we've never gotten the weather before
 		weather.timeUpdated < moment().format("x")-3600000 // or if the weather data is outdated
 	) {
-		getAndUpdateWeather(weather.location);
+		if (weather.weather_location_id != "") {
+			getAndUpdateWeather(weather.weather_location_id, true);
+		} else {
+			getAndUpdateWeather(weather.weather_location, false);
+		}
 	}
 });
 
@@ -102,17 +134,31 @@ $( document ).ready(function() {
 	updatePrefsDisplay("blur");
 	updateWeatherDisplay();
 
+	// add in all wallpaper thumbs
+	addWallpaperThumbs();
+
 	// fade in
 	$('.centered_box').fadeIn();
 	$('.wallpaper img').on("load", function(e) {
-		el = $(e.target);
-		parent = el.parent();
+		$(this).parent().fadeIn();
+	});
 
-		parent.fadeIn();
+	$('.reset_wallpaper_button').click(function() {
+		contentType = 'custom_image';
+
+		saveSetting(settings.contentTypes[contentType].default, contentType, true);
+		updatePrefsDisplay(contentType);
+	});
+	$('.reset_settings').click(function() {
+		confirmResetSettings();
 	});
 
 	$('.prefs_button').click(function() {
-		$('.prefs_panel').toggle();
+		$('.prefs_panel').stop().fadeToggle(200);
+	});
+
+	$('.prefs_panel').closest('.widget_outer').mouseleave(function() {
+		$(this).find('.prefs_panel').stop().fadeOut(200);
 	});
 
 	$('input[type="checkbox"]').change(function(e) {
@@ -122,18 +168,20 @@ $( document ).ready(function() {
 		saveSetting(this.checked, contentType, true);
 		updatePrefsDisplay(contentType);
 	});
-	$('[contentEditable]').on('keydown', function(e) {
+	$('[data-content-type]:not([type="checkbox"])').on('keydown', function(e) {
 		el = $(e.target);
 		contentType = el.attr('data-content-type');
+		text = getOrEditText(el, undefined, true);
 
-		// don't allow new lines if not allowed for the content type
+		// don't allow new lines if not allowed for the content type. instead, submit.
 		if (settings.contentTypes[contentType].allowNewlines === false && e.keyCode === 13) {
+			submitContentEditable(el);
 			return false;
 		}
 
 		// don't allow typing more characters if reached max
 		if (
-			settings.contentTypes[contentType].maxCharacters <= el.text().length
+			settings.contentTypes[contentType].maxCharacters <= text.length
 			&& e.key.length == 1 // this limits it to only keys that insert text
 			&& !(e.keyCode == 8 || e.keyCode == 46) // check to see if we're hitting backspace or delete
 			&& !(e.ctrlKey)
@@ -142,22 +190,20 @@ $( document ).ready(function() {
 			return false;
 		}
 	});
-	$('[contentEditable]').on('drop paste', function(e) {
+	$('[data-content-type]:not([type="checkbox"])').on('drop paste', function(e) {
 		el = $(e.target);
-		originalText = el.text();
+		originalText = getOrEditText(el, undefined, true);
 
 		// cancel paste
 		e.originalEvent.preventDefault();
 
 		// get text representation of clipboard
-		var oldText;
+		var text;
 		if (e.type == "paste") {
-			oldText = e.originalEvent.clipboardData.getData("text/plain");
+			text = e.originalEvent.clipboardData.getData("text/plain");
 		} else if (e.type == "drop") {
-			oldText = e.originalEvent.dataTransfer.getData("Text");
+			text = e.originalEvent.dataTransfer.getData("Text");
 		}
-		
-		var text = prepTextForDisplay(oldText);
 
 		// don't allow new lines if not allowed for the content type
 		if (settings.contentTypes[contentType].allowNewlines === false) {
@@ -170,29 +216,10 @@ $( document ).ready(function() {
 		}
 
 		// insert text manually
-		document.execCommand("insertHTML", false, text);
+		document.execCommand("insertHTML", false, convertLinebreaksToBrs(text));
 	});
-	$('[contentEditable]').blur(function(e) {
-		el = $(e.target);
-		contentType = el.attr('data-content-type');
-
-		// parse the text. convert <br/> to newlines
-		newSaveText = prepTextForSave(el.html());
-
-		// if blank, change to default text
-		if (newSaveText == "" || el.text() == "") {
-			newSaveText = settings.contentTypes[contentType].default;
-		}
-
-		newDisplayText = prepTextForDisplay(htmlDecode(newSaveText));
-		el.html(newDisplayText);
-
-		if (contentType == "weather_location") {
-			getAndUpdateWeather(newSaveText);
-		} else {
-			saveSetting(newSaveText, contentType, true);
-			updatePrefsDisplay(contentType);
-		}
+	$('[data-content-type]:not([type="checkbox"])').blur(function(e) {
+		submitContentEditable(this);
 	});
 
 	/*
@@ -202,6 +229,93 @@ $( document ).ready(function() {
 		});
 	*/
 });
+
+function addWallpaperThumbs() {
+	$.each(wallpapers, function(index, wallpaper) {
+		$('.wallpaper_thumbs').append(
+			$('<img>')
+				.attr('src', wallpaper.image+"/84x65")
+				.attr('value', wallpaper.image+"/1920x1080")
+				.attr('data-content-type', 'custom_image')
+				.click(function() {
+					submitContentEditable(this);
+				})
+		);
+	});
+}
+
+/*
+	params
+	- el -> element
+	- text -> text to set. undefined if we're just getting the text.
+	- isPlainText -> bool. true if getting/setting plain text (.text() instead of .html()). defaults to false
+*/
+function getOrEditText(el, text, isPlainText) {
+	if (typeof isPlainText == "undefined") {
+		isPlainText = false;
+	}
+
+	el = $(el);
+	tagName = el.prop('tagName');
+
+	toReturn = '';
+
+	if (tagName == "DIV" || tagName == "SPAN") {
+		if (typeof text == "undefined") {
+			toReturn = htmlDecode(convertBrsToLinebreaks(el.html()));
+		} else {
+			toReturn = el.html(convertLinebreaksToBrs(htmlEncode(text)));
+		}
+	} else if (tagName == "INPUT") {
+		if (typeof text == "undefined") {
+			toReturn = el.val();
+		} else {
+			toReturn = el.val(text);
+		}
+	} else if (tagName == "IMG") {
+		if (typeof text == "undefined") {
+			if (typeof el.attr('value') != "undefined") {
+				toReturn = el.attr("value");
+			} else {
+				toReturn = el.attr("src");
+			}
+		} else {
+			if (typeof el.attr('value') != "undefined") {
+				toReturn = el.attr("value", text);
+			} else {
+				toReturn = el.attr("src", text);
+			}
+		}
+	}
+
+	return $.trim(toReturn);
+}
+
+function submitContentEditable(el) {
+	el = $(el);
+	contentType = el.attr('data-content-type');
+	text = getOrEditText(el);
+
+	// if blank, change to default text
+	if (text == "") {
+		text = settings.contentTypes[contentType].default;
+	}
+
+	// if the value hasn't changed, don't do anything
+	if (
+		(contentType != "weather_location" && text == prefs[contentType])
+		|| (contentType == "weather_location" && text == weather.weather_location)
+	) {
+		return;
+	}
+
+	if (contentType == "weather_location") {
+		getAndUpdateWeather(text, false);
+	} else {
+		saveSetting(text, contentType, true);
+		updatePrefsDisplay(contentType);
+	}
+}
 
 function saveSetting(newText, key, isSync) {
 	prefs[key] = newText;
@@ -218,6 +332,19 @@ function saveSetting(newText, key, isSync) {
 	return newText;
 }
 
+function confirmResetSettings() {
+	var r = confirm("Are you sure you want to reset all your Minimalite settings?");
+	if (r == true) {
+		resetSettings();
+	}
+}
+
+function resetSettings() {
+	chrome.storage.sync.clear();
+	chrome.storage.local.clear();
+	location.reload();
+}
+
 function prepTextForDisplay(text) {
 	return htmlEncode(text).replace(/(?:\r\n|\r|\n)/g, '<br>');
 }
@@ -232,7 +359,9 @@ function updatePrefsDisplay(key) {
 	if (settings.contentTypes[key].type == "checkbox") {
 		els.attr("checked", prefs[key]);
 	} else {
-		els.html(prepTextForDisplay(prefs[key]));
+		$.each(els, function(index, el) {
+			getOrEditText(el, prefs[key]);
+		});
 	}
 
 	if (key == "custom_image") {
@@ -253,34 +382,52 @@ function updatePrefsDisplay(key) {
 }
 
 function removeLinebreaksForDisplay(text) {
-	return text.replace(/<\s*br.*?>/g, " ");
+	return text.replace(/(?:\r\n|\r|\n)/g, " ");
+	// .replace(/<\s*br.*?>/g, " ")
+}
+
+function convertLinebreaksToBrs(text) {
+	return text.replace(/(?:\r\n|\r|\n)/g, "<br>");
+}
+
+function convertBrsToLinebreaks(text) {
+	return text.replace(/<\s*br.*?>/g, "\n");
 }
 
 function htmlEncode(value){
-  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
-  //then grab the encoded contents back out.  The div never exists on the page.
-  return $('<div/>').text(value).html();
+	//create a in-memory div, set it's inner text(which jQuery automatically encodes)
+	//then grab the encoded contents back out.  The div never exists on the page.
+	newlineDelim = '%%%%%html_decode_newline%%%%%';
+	pattern = new RegExp(newlineDelim, 'g');
+	return $('<div/>').text(
+		value.replace(/(?:\r\n|\r|\n)/g, newlineDelim)
+	).html().replace(pattern, "\n");
 }
 
 function htmlDecode(value) {
-  return $('<div/>').html(value).text();
+	newlineDelim = '%%%%%html_decode_newline%%%%%';
+	pattern = new RegExp(newlineDelim, 'g');
+	return $('<div/>').html(
+		value.replace(/(?:\r\n|\r|\n)/g, newlineDelim)
+	).text().replace(pattern, "\n");
 }
 
-function getAndUpdateWeather(location) {
+function getAndUpdateWeather(location, isUsingWeatherId) {
 	console.log("Getting weather...");
-	$.simpleWeather({
-		location: location,
-		unit: 'f',
-		woeid: '',
-		success: function(weather) {
-			console.log("Weather received! Updating...");
-			updateWeather(weather);
-			updateWeatherDisplay();
-		},
-		error: function(error) {
-			console.log(error);
-			$('.weather_location_container').effect("highlight", {color: "ff9595"}, 2000);
-		}
+
+	if (isUsingWeatherId) {
+		param = 'id';
+	} else {
+		param = 'q';
+	}
+
+	$.get("http://api.k0shy.com/weather/?"+param+"="+location, function( data ) {
+		console.log("Weather received! Updating...");
+		updateWeather(data);
+		updateWeatherDisplay();
+	}).fail(function() {
+		console.log(error);
+		$('.weather_location_container').effect("highlight", {color: "ff9595"}, 2000);
 	});
 }
 
@@ -303,10 +450,12 @@ function updateWeather(weatherObj) {
 
 function updateWeatherDisplay() {
 	console.log("Updating weather display");
-	if (weather.city != "") {
+	if (weather.weather_location != "") {
 		$('.weather .weather_high').html(weather.high+"&deg;");
 		$('.weather .weather_low').html(weather.low+"&deg;");
-		$('.weather .weather_icon').attr("src", "/icons/weather/"+weather.todayCode+".svg");
-		$('.weather_location').html(weather.city);
+		$('.weather .weather_icon').removeClass(function(index, css) {
+			return (css.match(/(^|\s)wi-owm-\S+/g) || []).join(' ');
+		}).addClass('wi-owm-'+weather.condition_code);
+		$('.weather_location').html(weather.weather_location);
 	}
 }
